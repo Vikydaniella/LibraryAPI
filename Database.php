d9572cc83fefbdc977719468d509956534873973
Victoria Odeh <victoriaoderinde2016@gmail.com>
2023-04-13 17:10:38 +0100

    Connect to database

---
+++ Database.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "127.0.0.1";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  ("Connection failed: " . $conn->connect_error);
+}
+else {
+  echo "Connected successfully";
+}
