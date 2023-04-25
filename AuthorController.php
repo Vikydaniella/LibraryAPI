64bb2ba817f5a5b9b30325305402e9531ce41aa2
Victoria Odeh <victoriaoderinde2016@gmail.com>
2023-04-25 22:03:47 +0100

    Book Controller

---
+++ AuthorController.php
@@ -0,0 +1,14 @@
+<?php
+class AuthorController {
+    public function getAuthor() 
+    {require_once "./Database.php";
+        $sql = "SELECT * from books";
+        $result = $conn->query($sql);
+        $rows = array();
+        while ($r = mysqli_fetch_assoc($result)) {
+            $rows[] = $r;
+        }
+        echo json_encode($rows);
+
+    }
+}
