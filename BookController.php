461d30a64636cb2aa24a87d944fcefcb4e1ade79
victoriaoderinde <victoriaoderinde2016@gmail.com>
2023-05-09 22:03:28 +0100

    Update Books

--- BookController.php
+++ BookController.php
@@ -10,9 +10,14 @@ class BookController {
     }
   }
   elseif($method == "POST") {
+    if (isset($id)) {
+      $this->updateBook($id);
+    }
+    else{
     $this->createBooks();
   }
-  elseif($method == "POST") {
+}
+  elseif($method == "DELETE") {
     $this->deleteBook($id);
   }
   else{ 
@@ -66,9 +71,29 @@ if ($result=== TRUE) {
         $sql = "DELETE FROM books WHERE id='$id'";
         $result = $conn->query($sql);
         if ($result=== TRUE) {
-          echo "The record deleted @123successfully";
+          echo "The record was deleted successfully";
           } else {
             echo "Error: " . $sql . "<br>" . $conn->error;
           }
             }
+
+            private function updateBook(string $id) {
+              require_once "./Database.php";
+              //print_r($_POST);
+              //exit;
+              $title = $_POST['title'];
+              $author = $_POST['author'];
+              $year_of_publication = $_POST['year_of_publication'];
+              $category = $_POST['category'];
+              $isbn = $_POST['isbn'];
+              $quantity = $_POST['quantity'];
+              $sql = "UPDATE books SET title = $title, author = $author, year_of_publication = $year_of_publication,
+               category =$category, isbn =$isbn, quantity =$quantity, WHERE id='$id'";
+              $result = $conn->query($sql);
+                if ($result=== TRUE) {
+                  echo "The record was deleted successfully";
+                  } else {
+                    echo "Error: " . $sql . "<br>" . $conn->error;
+                  }
+                    }
 }
